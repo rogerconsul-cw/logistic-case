0e09c8492d473af2618d58894fb4c100a3ebd702
Roger C <roger.consul@cloudwalk.io>
2023-09-25 22:06:53 -0300

    basic book test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  setup do
+    @author = authors(:one)  # assuming you have a fixture for authors
+  end
+
+  test "should be valid with a title, author, and publication_year" do
+    book = Book.new(title: "Sample Title", author: @author, publication_year: 2022)
+    assert book.valid?
+  end
+
+  test "should be invalid without a title" do
+    book = Book.new(title: nil, author: @author, publication_year: 2022)
+    assert_not book.valid?
+  end
+
+  test "should be invalid without an author" do
+    book = Book.new(title: "Sample Title", author:
