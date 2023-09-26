43b938ce30a6468988ae9cd6d9e8472500f3c093
Roger C <roger.consul@cloudwalk.io>
2023-09-25 22:00:10 -0300

    Initial DB setup and seed. Now we shall start TDD.

--- db/migrate/20230926005102_create_books.rb
+++ db/migrate/20230926005102_create_books.rb
@@ -1,6 +1,16 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
+      t.string :title
+      t.string :isbn
+      t.integer :page_count
+      t.datetime :published_date
+      t.string :thumbnail_url
+      t.text :short_description
+      t.text :long_description
+      t.string :status
+      t.references :author, null: false, foreign_key: true
+      t.integer :publication_year
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+
+# Read the JSON file from app/assets
+file_path = Rails.root.join('app', 'assets', 'books.json')
+books_json = File.read(file_path)
+
+parsed_books = JSON.parse(books_json)["books"]
+
+parsed_books.each do |book_data|
+  author = Author.find_or_create_by(name: book_data["author"])
+  book = Book.new(
+    title: book_data["title"],
+    isbn: book_data["isbn"],
+    page_count: book_data["pages"],
+    published_date: book_data["published"],
+    publication_year: book_data["published"].to_date.year,
+    author_id: author.id
+  )
+  book.save
+end
