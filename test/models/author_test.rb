23445aca9384f85ab55b9cd5f6e6f82d6cc79ab2
Roger C <roger.consul@cloudwalk.io>
2023-09-25 22:06:46 -0300

    basic author test

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with a name" do
+    author = Author.new(name: "John Doe")
+    assert author.valid?
+  end
+
+  test "should be invalid without a name" do
+    author = Author.new(name: nil)
+    assert_not author.valid?
+  end
+
+  test "should be invalid with a duplicate name" do
+    Author.create(name: "John Doe")
+    author = Author.new(name: "John Doe")
+    assert_not author.valid?
+  end
 end
