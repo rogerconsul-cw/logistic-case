f62104ba21167dd8070f4af7e34caada1a51b24a
Roger C <roger.consul@cloudwalk.io>
2023-09-28 22:42:12 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'front_page#front_page'
+
+  get 'search', to: 'front_page#search'
+
   resources :authors
   resources :books
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
